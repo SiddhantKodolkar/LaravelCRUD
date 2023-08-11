f1d8c7e76f2301e9213176dd727c8a8b6fa72fa9
Siddhant Kodolkar <kodolkar.siddhant19@gmail.com>
2023-08-11 11:19:38 +0530

    added auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 /*
@@ -8,8 +9,8 @@ use App\Http\Controllers\ProductController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -17,9 +18,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/product',[ProductController::class,'index'])->name('products.index');
-Route::get('/product/create',[ProductController::class,'create'])->name('products.create');
-Route::post('/product',[ProductController::class,'store'])->name('products.store');
-Route::get('/product/{product}/edit',[ProductController::class,'edit'])->name('products.edit');
-Route::put('/product/{product}/update',[ProductController::class,'update'])->name('products.update');
-Route::delete('/product/{product}/destroy',[ProductController::class,'destroy'])->name('products.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    //adding this in middleware so that the routes are protected and only the authenticated users can access this route
+    Route::get('/product',[ProductController::class,'index'])->name('products.index');
+    Route::get('/product/create',[ProductController::class,'create'])->name('products.create');
+    Route::post('/product',[ProductController::class,'store'])->name('products.store');
+    Route::get('/product/{product}/edit',[ProductController::class,'edit'])->name('products.edit');
+    Route::put('/product/{product}/update',[ProductController::class,'update'])->name('products.update');
+    Route::delete('/product/{product}/destroy',[ProductController::class,'destroy'])->name('products.destroy');
+});
+
+require __DIR__.'/auth.php';
